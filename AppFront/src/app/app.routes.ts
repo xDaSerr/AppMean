449288a87ae222a124c2850541c5e6e2bc90f6fd
Jose-ac21 <a21490516@itmexicali.edu.mx>
2026-02-18 19:40:18 -0600

    Estilos y componentes songs y home

--- AppFront/src/app/app.routes.ts
+++ AppFront/src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { SongsList } from './componentes/songs/songs-list/songs-list';
 import { DetailSong } from './componentes/songs/detail-song/detail-song';
 import { AddSong } from './componentes/songs/add-song/add-song';
+import { Home } from './componentes/home/home';
 
 export const routes: Routes = [
     {path: 'songs', component: SongsList},
     {path: 'songs/add', component: AddSong},
+    {path: '', component: Home },
     {path: 'songs/:songId', component: DetailSong}
 ];
+
