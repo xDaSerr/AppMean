203e2259e310d548b67b2b1553729604bb93a950
Jose-ac21 <a21490516@itmexicali.edu.mx>
2026-02-18 19:16:30 -0600

    Componente home y estilos

--- AppFront/src/app/componentes/songs/songs-list/songs-list.ts
+++ AppFront/src/app/componentes/songs/songs-list/songs-list.ts
@@ -1,13 +1,13 @@
 import { Component, inject, signal } from '@angular/core';
 import { SongsService } from '../../../services/songs';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { RouterLink } from "@angular/router";
 
 
 @Component({
   selector: 'app-songs-list',
   standalone: true,
-  imports: [NgFor, RouterLink],
+  imports: [NgFor, NgIf, RouterLink],
   templateUrl: './songs-list.html',
   styleUrl: './songs-list.css',
 })
